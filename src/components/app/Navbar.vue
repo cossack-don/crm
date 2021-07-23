da0142559a883706b8a645062e3278720043611a
SergeyZhur <ser90593696@yandex.ru>
2021-07-23 15:21:39 +0300

    добавил компонент время в header , сделал фильтр для времени. Добавил дроп-даун в header для логаута и профиля

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,7 +5,8 @@
         <a href="#" @click.prevent="$emit('click')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <!-- фильтр даты -->
+        <span class="black-text">{{ date | date("datetime") }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -14,6 +15,7 @@
             class="dropdown-trigger black-text"
             href="#"
             data-target="dropdown"
+            ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +23,13 @@
 
           <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="loguot">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -37,3 +39,44 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null,
+  }),
+
+  methods: {
+    loguot() {
+      // переходим на страницу логин
+      this.$router.push("/login?message=logout");
+    },
+  },
+
+  mounted() {
+    // время в header
+    // обновляем каждую секунду
+    this.interval = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+
+    // для дропдауна materialise css
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false,
+    });
+  },
+
+  // удаляем компонент - block. хуком при переходе на стр логин
+  beforeDestroy() {
+    // очищаем время - удаляем из памяти
+    clearInterval(this.interval);
+
+    // если существует приватная переменная и есть метод дестрой, то вызываем
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// нативные компоненты
+// компонент dateFilter - для часов в header
+import dateFilter from '@/filters/date.filters.js'
+
 // javascript из materialize
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+
+// регистрируем наш собственный фильтер
+Vue.filter('date', dateFilter);
 
 new Vue({
   router,
