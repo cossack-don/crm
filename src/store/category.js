fda696816b118cc9c4316d37a3c117c055a5d9d4
SergeyZhur <ser90593696@yandex.ru>
2021-07-30 11:22:58 +0300

    доделал карточку подробней о доходах/расходах

--- src/store/category.js
+++ src/store/category.js
@@ -66,7 +66,25 @@ export default {
                 commit('setError',e)
                 throw e
             }
-        }
+        },
+        // 
+
+        async fetchCategoryById({commit, dispatch}, id) {
+            try {
+                // получаем id
+            const uid = await dispatch('getUid')
+    
+            // получаем категории из firebase 
+           const category = (await firebase.database().ref(`/users/${uid}/categories`).child(id).once('value')).val() || {}
+  
+            return {...category,id}
+            
+        
+        } catch(e) {
+                commit('setError',e)
+                throw e
+            }
+        },
     },
 
 }
