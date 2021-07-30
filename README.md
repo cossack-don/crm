fca65a8bb70e81ae2afedca9aa8c176a3ac67abe
SergeyZhur <ser90593696@yandex.ru>
2021-07-30 15:16:13 +0300

    изменил верстку на странице profile, добавил диаграмму для расходов/доходов с помощью vue chartjs

--- README.md
+++ README.md
@@ -1,31 +1,83 @@
-# crm
-page-auth
+# CRM system 
+
+```
+Описание приложения.
+```
+
+```
+Стек.
+JavaScript, Vue - 2, Firebase (database, auth), API Fixer (Валюты), Vue chart.js
+```
+<!-- 1 -->
+```
+Страница авторизации/регистрации, валидация
+```
 
 ![screenshot_1](preview-screen-shot/auth.jpg)
 
-page-main 
 
+<!-- 2 -->
+```
+Страница баланса денег у данного пользователя и корректные данные по валютам (RUB, USD, EUR)
+```
 ![screenshot_2](preview-screen-shot/main-page.jpg)
 
 
+<!-- 3 -->
+```
+Страница история записей. Показывает расходы/доходы пользователя, можно посмотреть подробней. Пагинация.
+```
+![screenshot_2](preview-screen-shot/page-history.jpg)
 
-## Project setup
+
+<!-- 4 -->
+```
+Страница планирования. Показываем актуальный баланс, показываем прогресс расходов и доходов.
+```
+![screenshot_2](preview-screen-shot/planning-page.jpg)
+
+
+<!-- 5 -->
+```
+Страница новая запись. Можно создать новую категорию дохода/расхода, указать сумму и описание.
+```
+
+![screenshot_2](preview-screen-shot/createPost-page.jpg)
+
+
+<!-- 6 -->
+```
+Страница категории. Можно создать категорию и указать баланс, сколько будет лежать финансов.
+Есть возможность подкорректировать указанную категорию и баланс.
+```
+
+![screenshot_2](preview-screen-shot/category-page.jpg)
+
+<!-- 7 -->
+```
+Страница профиль. Смена имени и radio кнопка для смены языка ru/en.
+```
+
+<!-- не сделал еще -->
+<!-- ![screenshot_2](preview-screen-shot/category-page.jpg) -->
+
+
+
+## Установка зависимостей
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Запуск проекта dev
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
+### Сборка проекта для prod
 ```
 npm run build
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
-
 <!-- https://fixer.io/quickstart  - для валюты АПИ до 1 000 запросов в месяц -->
 <!-- дают ключ при регистрации -->
+
--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading" />
@@ -35,38 +35,99 @@
 import HistoryTable from "@/components/HistoryTable";
 import paginationMixin from "@/mixins/pagination.mixin.js";
 
+// плагин для графиков vue chart js
+
+// могут быть ошибки от новых версий см ссылку https://best-itpro.ru/news/oshibka_export_default_imported_as_chart_was_not_found_in_chart_js/
+//export 'default' (imported as 'Chart') was not found in 'chart.js'
+// ... т.е. не удается найти модуль 'chart.js'
+// Так вот, возникает она из-за того, что версии vue-chartjs не поспевают за последними версиями Chart.js.
+//  А когда вы запускаете команду npm install vue-chartjs chart.js --save в своём проекте, то на выходе получаете самые последние версии vue-chartjs и chart.js,
+//  у которых может быть несовместимость.
+// Решение очень простое - вам нужно просто понизить версию chart.js, и всё!
+//
+// https://vue-chartjs.org/ru/
+// https://www.chartjs.org/docs/latest/#creating-a-chart
+// https://codepen.io/apertureless/pen/zEvvWM?editors=1010
+import { Pie } from "vue-chartjs";
+
 export default {
   name: "history",
+  //
+  extends: Pie,
+  //
   mixins: [paginationMixin],
   //
   async mounted() {
     this.records = await this.$store.dispatch("fetchRecords");
-
+    // const records = await this.$store.dispatch("fetchRecords");
     const categories = await this.$store.dispatch("fetchCategories");
 
-    // const records = await this.$store.dispatch("fetchRecords");
-
-    this.setupPagination(
-      this.records.map((record) => {
-        return {
-          ...record,
-          categoryName: categories.find((c) => c.id === record.categoryId)
-            .title,
-          typeClass: record.type === "income" ? "green" : "red",
-          typeText: record.type === "income" ? "Доход" : "Расход",
-        };
-      })
-    );
+    // метод
+    this.setup(categories);
 
     this.loading = false;
   },
   data: () => ({
     loading: true,
     records: [],
-    // categories: [],
   }),
+  //
   components: {
     HistoryTable,
+  },
+  //
+  methods: {
+    setup(categories) {
+      this.setupPagination(
+        this.records.map((record) => {
+          return {
+            ...record,
+            categoryName: categories.find((c) => c.id === record.categoryId)
+              .title,
+            typeClass: record.type === "income" ? "green" : "red",
+            typeText: record.type === "income" ? "Доход" : "Расход",
+          };
+        })
+      );
+
+      // плагин vue chart js для графиков
+      this.renderChart({
+        labels: categories.map((c) => c.title),
+        datasets: [
+          {
+            label: "Расходы по категориям",
+            data: categories.map((c) => {
+              return this.records.reduce((total, r) => {
+                if (r.categoryId === c.id && r.type === "outcome") {
+                  total += r.amount;
+                }
+                return total;
+              }, 0);
+            }),
+            // цвета для кол-ва пользователей, для диаграммы, цвет и обводка
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(225, 151, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+              "rgba(125, 051, 64, 0.2)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      });
+    },
   },
 };
 </script>
